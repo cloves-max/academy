fb104c60c36c30e91810c608e4aa81fd9c0b469b
cloves-max <clovesjuniorrodrigues@gmail.com>
2022-05-05 19:54:10 -0300

    edit telas

--- paginas/home/home.php
+++ paginas/home/home.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,17 +12,19 @@
 
 
 <div class="adiscent">
-    <button class="button button2"><a href="index_admin.php?menuop=createdisc">ADD Discente</a></button>   
+    <button class="button button2"><a href="index_admin.php?menuop=createdisc">ADD Discente</a></button>
 </div>
+
 <body>
-<table id="customers">
-  <tr>
-    <th>Nome</th>
-    <th>N° Matricula</th>
-    <th>Idade</th>
-    <th>Sexo</th>
-  </tr>
-            
+    <table id="customers">
+        <tr>
+            <th>Nome</th>
+            <th>N° Matricula</th>
+            <th>Idade</th>
+            <th>Sexo</th>
+            <th >mover pra direita Ações</th>
+        </tr>
+
         </tr>
         <?php
         include('db/conexao.php');
@@ -33,12 +34,25 @@
 
         ?>
             <tr>
-                <td><?= $discentes['nome']?></td>
-                <td><?= $discentes['matricula']?></td>
-                <td><?= $discentes['idade']?></td>
-                <td><?= $discentes['sexo']?></td>
+                <td><?= $discentes['nome'] ?></td>
+                <td><?= $discentes['matricula'] ?></td>
+                <td><?= $discentes['idade'] ?></td>
+                <?php
+                if( $discentes['sexo']=='M'){?>
+                    <td>Masculino</td>
+                <?php
+                }else{?>
+                    <td>Femino</td>
+                <?php    
+                }
+                ?>
+               
+                <td id="butonsfim">
+                    <button class="button button3">edit</button>
+                    <button class="button button4">delet</button>
+                </td>
                 
-                <td>editar</td>
+
             </tr>
         <?php
         }
